0c4103838c97d4d4bb065f87c165f30170d572fc
Jairo Daniel Samaniego Villacres <jdsamaniego2@espe.edu.ec>
2021-05-05 15:14:55 -0500

    Corregido error en Spring Boot(No se puede acceder a las HKEY_CURRENT_USER al instalar como servicio de windows el .jar, tampoco se puede mostrar GUI porque los servicios se ejecutan en un entorno aislado de la GUI)

--- Backend Spring/src/main/java/com/elrasosonico/gdc/api/GeneradorDocumentosController.java
+++ Backend Spring/src/main/java/com/elrasosonico/gdc/api/GeneradorDocumentosController.java
@@ -7,7 +7,6 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
-import java.io.InputStream;
 
 
 import static com.elrasosonico.gdc.configuraciones.Constantes.*;
@@ -21,28 +20,6 @@ public class GeneradorDocumentosController {
     public String generarFicha(@RequestBody Ficha ficha) throws IOException{
         //CREA EL ARCHIVO
         GeneradorDocumentos.generarDocumento(ficha.nombre_1+" "+ficha.nombre_2+" "+ficha.apellido_1+" "+ficha.apellido_2 +" "+ ficha.cedula,"Archivo.txt");
-
-        //OBTENIENDO RUTA DE MIS DOCUMENTOS
-        String documentos = null;
-        try {
-            Process p =  Runtime.getRuntime().exec("reg query \"HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Shell Folders\" /v personal");
-            p.waitFor();
-
-            InputStream in = p.getInputStream();
-            byte[] b = new byte[in.available()];
-            in.read(b);
-            in.close();
-
-            documentos = new String(b);
-            documentos = documentos.split("\\s\\s+")[4];
-
-        } catch(Throwable t) {
-            t.printStackTrace();
-        }
-
-        //ABRE EL DIRECTORIO DEL ARCHIVO
-        GeneradorDocumentos.abrirDirectorio(documentos);
-
         return ficha.toString();
     }
 
